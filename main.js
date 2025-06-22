f867de4d5d1d7da1303383bd481bbbd5296c7e8c
zikra-abdusemed-cmd <zikramohammed00@gmail.com>
2025-06-22 20:43:53 +0300

    image,titles and artists

---
+++ main.js
@@ -0,0 +1,30 @@
+import * as THREE from 'three';
+import { Reflector } from 'three/addons/objects/Reflector.js';
+import * as TWEEN from 'tween';
+
+const images = [
+  'AIDA_MULUNEH_I-IN-THE-OTHER_2017.jpg',
+  'AIDA_MULUNEH_BOTH-SIDES_2017.jpg',
+  'THE-DEW-AT-DAWN_19_AIDA_MULUNEH_FN_60X80-scaled.jpg',
+  'AIDA-MULUNEH_The-barriers-within_2021-scaled.jpg',
+  'AIDA_MULUNEH_EVERYBODY-KNOWS-ABOUT-MISSISSIPPI_2017.jpg',
+  'This-is-Where-I-am-Cover.jpg'
+];
+
+const titles = [
+  'I IN THE OTHER',
+  'BOTH SIDES',
+  'THE DEW AT DAWN',
+  'The-barriers',
+  'EVERYBODY KNOWS ABOUT MISSISSIPPI',
+  'This is Where I am Cover'
+];
+
+const artists = [
+  'Aida Muluneh',
+  'Aida Muluneh',
+  'Aida Muluneh',
+  'Aida Muluneh',
+  'Aida Muluneh',
+  'Aida Muluneh',
+];
