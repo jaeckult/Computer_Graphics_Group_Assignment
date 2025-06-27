7cf3f8bdb30014c3aa5802c19d2e0fc506a5da82
YITBAREK ALEMU <160623517+jaeckult@users.noreply.github.com>
2025-06-28 00:24:14 +0300

    fixed vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@
 * @type {import('vite').UserConfig}
 */
 export default {
-  // Set the base directory for GitHub pages
-  base: process.env.NODE_ENV === 'production' ? '/threejs-image-gallery/' : '/',
+  // Remove GitHub Pages base path for Vercel deployment
+  base: '/',
   build: {
     outDir: './dist',
     sourcemap: true,
